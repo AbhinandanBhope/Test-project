a54013df79ee9f72b66977f61200f19b6c196bd9
Abhinandan Bhopenode <abhinandanbhope1642000@gmail.comnode>
2023-05-15 00:25:21 +0530

    product cart relationship

---
+++ 08-fetching-related-products/util/database.js
@@ -0,0 +1,7 @@
+const Sequelize  = require('sequelize');
+const sequelize = new Sequelize('node-complete','root','Abhi@123',
+{
+    dialect:'mysql',
+host:'localhost'
+});
+ module.exports =sequelize;
